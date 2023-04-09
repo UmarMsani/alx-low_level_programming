b43b6700650810299c93784fe6b7f34d201f9589
UmarMsani <umuhammad220@gmail.com>
2023-04-09 02:15:09 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,9 +13,9 @@ int main(int argc, char *argv[])
 {
 	int j, count = 0;
 
-	if (argc < 1)
+	if (argc == 1)
 	{
-		printf("0\n");
+		printf("%d\n", 0);
 		return (0);
 	}
 	for (j = 1; j < argc; j++)

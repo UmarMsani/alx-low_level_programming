2997d3610f4bd6ee541847ab4731462f11efb2d2
UmarMsani <umuhammad220@gmail.com>
2023-04-10 07:04:55 -0700

    program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,10 +3,10 @@
 #include <ctype.h>
 
 /**
- * main - adds positive numbers
+ * main - adds positive and negative numbers
  * @argc: stores num of cmd-line arg
- * @argv: is array of char poiters
- * Return: 0 if no arg passed, 1 if char arg passed
+ * @argv: is array of char pointers
+ * Return: 0 if no arg passed, 1 if invalid arg passed
  */
 
 int main(int argc, char *argv[])
@@ -14,20 +14,35 @@ int main(int argc, char *argv[])
 	int j, count = 0;
 
 	if (argc == 1)
-		{
+	{
 		printf("%d\n", 0);
 		return (0);
-		}
+	}
 	for (j = 1; j < argc; j++)
+	{
+		int num = 0;
+		int i = 0;
+		int sign = 1;
+
+		if (argv[j][0] == '-')
 		{
-		if (isdigit(argv[j][0]))
-			count += atoi(argv[j]);
-		else
-			{
-			printf("Error\n");
-			return (1);
-			}
+			sign = -1;
+			i = 1;
 		}
+
+		for (; argv[j][i] != '\0'; i++)
+		{
+			if (!isdigit(argv[j][i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			num = num * 10 + (argv[j][i] - '0');
+		}
+
+		count += num * sign;
+	}
 	printf("%d\n", count);
 	return (0);
 }

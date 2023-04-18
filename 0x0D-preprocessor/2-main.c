69efbbac6f142982547be380b7980f5ea919e119
UmarMsani <umuhammad220@gmail.com>
2023-04-18 09:11:42 -0700

    2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* Description: this program prints the name of the file it was comipiled from
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

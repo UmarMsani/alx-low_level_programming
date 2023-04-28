2fe6b4926058cfb9c4a9a9d775c259f0f4246006
UmarMsani <umuhammad220@gmail.com>
2023-04-28 11:50:54 -0700

    Prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -13,12 +13,11 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s {
-    char *str;
-    int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);

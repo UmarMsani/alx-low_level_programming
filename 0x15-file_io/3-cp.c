400a77fc6e43f15ced7df2dd0c2d929dde9f1f1d
UmarMsani <umuhammad220@gmail.com>
2023-05-08 16:07:41 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,42 +8,43 @@
 int main(int argc, char *argv[])
 {
 	char buf[1024];
-	int fd_from, fd_to, nread;
+	int fd_from, fd_to;
+	ssize_t nread;
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
-		return (1);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		return (97);
 	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1 || access(argv[1], R_OK) == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-		return 2;
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
 	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd_to == -1)
 	{
-		fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		return 3;
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		return (99);
 	}
-	while ((nread = read(fd_from, buf, sizeof(buf))) > 0)
+	while ((nread = read(fd_from, buf, 1024)) > 0)
+	{
 		if (write(fd_to, buf, nread) != nread)
 		{
-			fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			return 4;
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			return (99);
 		}
+	}
 	if (nread == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		return 5;
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
 	}
-	close(fd_from);
-	close(fd_to);
-	return 0;
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: %d\n", fd_from < fd_to ? fd_from : fd_to);
+	return (100);
+	}
+	return (0);
 }

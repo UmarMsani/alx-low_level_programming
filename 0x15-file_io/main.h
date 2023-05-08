a59cb23122000694888ff799b2480b7b34c252f3
UmarMsani <umuhammad220@gmail.com>
2023-05-08 11:55:44 -0700

    Prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,9 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);

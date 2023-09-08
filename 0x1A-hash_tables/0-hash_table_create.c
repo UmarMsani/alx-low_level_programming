04342cd02e28402a5dca51f73c0187b9c2a7843b
UmarMsani <umuhammad220@gmail.com>
2023-09-07 23:15:02 -0700

    C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,9 +16,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_table->size = size;
+
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
 

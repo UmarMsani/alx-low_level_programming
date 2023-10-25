8cdb067302e5232431f82dbaf88908082008de78
UmarMsani <umuhammad220@gmail.com>
2023-10-25 12:13:12 -0700

    update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-""" function that def island_perimeter(grid):that returns the perimeter of the island described in grid"""
-
 
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in the grid.
-    
+
     Args:
         grid (List[List[int]]): 2D grid representing the island.
             0 represents a water zone, 1 represents a land zone.
-    
+
     Returns:
         int: Perimeter of the island.
     """
     rows = len(grid)
     cols = len(grid[0])
-    
+
     perimeter = 0
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
@@ -26,5 +24,5 @@ def island_perimeter(grid):
                     perimeter -= 2
                 if j > 0 and grid[i][j-1] == 1:
                     perimeter -= 2
-    
+
     return perimeter
